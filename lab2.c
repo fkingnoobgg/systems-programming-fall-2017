d458f6e9dbaf40835420a272eba4f9462ca9935a
0x0 <ghstoreinvesting@gmail.com>
2017-08-17 12:54:36 +0800

    add

--- lab2.c
+++ lab2.c
@@ -27,4 +27,51 @@ int main(int argc, char *argv[])
     int max = maxNumFromArray(numArr, argc-2);
     printf("Max num from given numbers is %i\n", max);
   }
+  else if (strcmp(argv[1], "ass1") == 0) {
+    int nspace = 4;
+    int nass = 1;
+    for (int i = 0; i < 5; i++)
+    {
+      for (int j = 0; j < nass; j++) {
+        printf("*");
+      }
+
+      for (int j = 0; j < nspace; j++) {
+        printf(" ");
+      }
+      nspace--;
+      nass++;
+      printf("\n");
+    }
+  }
+  else if (strcmp(argv[1], "ass4") == 0) {
+    int lspace = 4;
+    int rspace = 4;
+
+    int nass = 1;
+    for (int i = 0; i < 5; i++)
+    {
+      for (int j = 0; j < lspace; j++) {
+        printf(" ");
+      }
+
+      for (int j = 0; j < nass; j++) {
+        printf("*");
+      }
+
+      for (int j = 0; j < rspace; j++) {
+        printf(" ");
+      }
+
+      lspace--;
+      rspace--;
+      nass++;
+      nass++;
+
+      printf("\n");
+    }
+  }
+  else if (strcmp(argv[1], "ordinal") == 0) {
+    
+  }
 }

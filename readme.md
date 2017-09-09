108559b37d496db8ef40792e5276f626425d2fdb
0x0 <ghstoreinvesting@gmail.com>
2017-09-09 18:16:40 +0800

    update readme and add test file

--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 # Personal solutions to labwork relating to systems programming fall of 2017
 Course can be accessed here: http://teaching.csse.uwa.edu.au/units/CITS2002/
 Though the creator can change or remove the content there anytime (happens often)
+
+## Compiling
+Using visual studio C++ compiler (Developer Command Prompt)
+`cl /EHsc hello.cpp`
+Running it
+`hello`
+
+On Mac
+`cc -std=c99 -Wall -Werror -pedantic -o hello hello.c`
---
+++ test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void vFunc(int x) {
+    int x = 5;
+}
+
+
+int main(int argc, char const *argv[]) {
+    /* code */
+    // for (int i = 10; i > 0; --i)
+    // {
+    //     printf("%i\n", i);
+    // }
+    // return 0;
+    // int buffer[100];
+    // int x = sizeof buffer;
+
+    //int x = sizeof "moose";
+    // int x = atoi(argv[1]);
+    srand(time(NULL));
+    printf("%i\n", rand());
+}
